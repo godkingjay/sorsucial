417f29d42972e094f2254aa885c820e7c4e46c25
Jarrian <glorianagojar@gmail.com>
2023-03-02 20:08:03 +0800

    feat: remove unused variables and imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,26 +1,11 @@
 import "@/styles/globals.scss";
-
-import { NextPage } from "next";
-
 import type { AppProps } from "next/app";
-
-import { ReactElement, ReactNode } from "react";
 import { RecoilRoot } from "recoil";
 
-export type NextPageLayout<P = {}, IP = P> = NextPage<P, IP> & {
-	getLayout?: (page: ReactElement) => ReactNode;
-};
-
-type AppPropsWithLayout = AppProps & {
-	Component: NextPageLayout;
-};
-
-export default function App({ Component, pageProps }: AppPropsWithLayout) {
-	const getLayout = Component.getLayout ?? ((page) => page);
-
+export default function App({ Component, pageProps }: AppProps) {
 	return (
 		<RecoilRoot>
-			<>{getLayout(<Component {...pageProps} />)}</>
+			<Component {...pageProps} />
 		</RecoilRoot>
 	);
 }

9be309d850108b3ad1f576ab1b62255c3a5d46f2
Jarrian <glorianagojar@gmail.com>
2023-03-03 19:31:32 +0800

    feat: dont render main layout when not authenticated

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,23 @@
 import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+	
 	return (
 		<RecoilRoot>
-			<Layout>
-				<Component {...pageProps} />
-			</Layout>
+			{
+				router.pathname.split("/")[1].match(/auth/g) ? (
+					<Component {...pageProps} />
+				) : (
+					<Layout>
+						<Component {...pageProps} />
+					</Layout>
+				)
+			}
 		</RecoilRoot>
 	);
 }

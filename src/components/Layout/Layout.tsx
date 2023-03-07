afbd25ccc8991e919875051dff97126b41bf4138
Jarrian <glorianagojar@gmail.com>
2023-03-07 11:14:16 +0800

    Update Layout component to add loading states when user is being fetched from Firebase

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,18 +1,26 @@
 import React from "react";
 import useUser from "@/hooks/useUser";
 import NavBar from "../NavBar/NavBar";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth } from "@/firebase/clientApp";
 
 type LayoutProps = {
 	children: React.ReactNode;
 };
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const {} = useUser();
+	const { authUser, loadingUser, authLoading, setLoadingUser } = useUser();
 
 	return (
 		<main className="scroll-y-style flex flex-col max-h-screen h-screen overflow-y-auto relative">
-      <NavBar />
-			{children}
+			{authLoading || loadingUser ? (
+				<p>Loading</p>
+			) : (
+				<>
+					<NavBar />
+					{children}
+				</>
+			)}
 		</main>
 	);
 };

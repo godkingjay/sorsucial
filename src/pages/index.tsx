2164bd0a750689f98178b22b025c34db626b8c6d
Jarrian <glorianagojar@gmail.com>
2023-03-07 11:14:46 +0800

    Remove unnecessary tags from Index component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,24 +1,7 @@
-import Head from "next/head";
-
 export default function Home() {
 	return (
 		<>
-			<Head>
-				<title>SorSUcial</title>
-				<meta
-					name="description"
-					content="SorSUcial is the unofficial social media for the University of Sorsogon State University."
-				/>
-				<meta
-					name="viewport"
-					content="width=device-width, initial-scale=1"
-				/>
-				<link
-					rel="icon"
-					href="/assets/logo/sorsu-xs.png"
-				/>
-			</Head>
-			<main></main>
+			<main>Home</main>
 		</>
 	);
 }

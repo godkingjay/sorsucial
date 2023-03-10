cb7e776b7723b3276ce7702f415ec9fb9ed1cdbe
Jarrian <glorianagojar@gmail.com>
2023-03-10 19:31:34 +0800

    chore:    Remove useRouter import from _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import Layout from "@/components/Layout/Layout";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { useRouter } from "next/router";
 import { RecoilRoot } from "recoil";
 
 export default function App({ Component, pageProps }: AppProps) {

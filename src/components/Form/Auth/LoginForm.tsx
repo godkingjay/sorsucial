9592d30f8e4288f523874f1d75d959d700e8b0fc
Jarrian <glorianagojar@gmail.com>
2023-03-10 10:33:45 +0800

    feat:  Add show password toggle to LoginForm

--- src/components/Form/Auth/LoginForm.tsx
+++ src/components/Form/Auth/LoginForm.tsx
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 type LoginFormProps = {};
 
 const LoginForm: React.FC<LoginFormProps> = () => {
+	const [showPassword, setShowPassword] = useState(true);
+
+	const handleShowPassword = () => {
+		setShowPassword((prev) => !prev);
+	};
+
 	return (
 		<form className="auth-form">
 			<h2 className="font-bold w-full text-center text-2xl text-logo-500 py-4 uppercase">
@@ -13,37 +20,53 @@ const LoginForm: React.FC<LoginFormProps> = () => {
 				<div className="flex flex-col flex-1 gap-y-6">
 					<div className="w-full flex flex-col gap-y-4 flex-1 justify-center">
 						<div className="w-full flex flex-col">
-							<div className="auth-input-text">
-								<label
-									htmlFor="email"
-									className="label"
-								>
-									EMAIL
-								</label>
-								<input
-									required
-									type="email"
-									name="email"
-									title="Email"
-									className="input-field"
-								/>
+							<div className="auth-input-container">
+								<div className="auth-input-text">
+									<label
+										htmlFor="email"
+										className="label"
+									>
+										EMAIL
+									</label>
+									<input
+										required
+										type="email"
+										name="email"
+										title="Email"
+										className="input-field"
+									/>
+								</div>
 							</div>
 						</div>
 						<div className="w-full">
-							<div className="auth-input-text">
-								<label
-									htmlFor="password"
-									className="label"
+							<div className="auth-input-container">
+								<div className="auth-input-text">
+									<label
+										htmlFor="password"
+										className="label"
+									>
+										PASSWORD
+									</label>
+									<input
+										required
+										type={showPassword ? "text" : "password"}
+										name="password"
+										title="Password"
+										className="input-field"
+									/>
+								</div>
+								<button
+									type="button"
+									title={showPassword ? "Hide Password" : "Show Password"}
+									className="aspect-square h-5 w-5 [&:hover_path]:fill-logo-300 [&:focus_path]:fill-logo-300"
+									onClick={handleShowPassword}
 								>
-									PASSWORD
-								</label>
-								<input
-									required
-									type="password"
-									name="password"
-									title="Password"
-									className="input-field"
-								/>
+									{showPassword ? (
+										<AiFillEye className="h-full w-full" />
+									) : (
+										<AiFillEyeInvisible className="h-full w-full" />
+									)}
+								</button>
 							</div>
 						</div>
 					</div>

d79c141a86dccd465df3f7c360f0de3a81ce1c7e
Jarrian <glorianagojar@gmail.com>
2023-03-10 17:48:52 +0800

    feat:

---
+++ src/lib/interfaces/staff.ts
@@ -0,0 +1,45 @@
+import { Timestamp } from "firebase/firestore";
+
+/**
+ * This interface is used to define the structure of a staff object.
+ * This is the object that is stored in the database.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @export
+ * @interface Staff
+ * @category Interfaces
+ *
+ * ----------------------------------------------------------------
+ *
+ * @property {string} staffId - The unique identifier of the staff.
+ * @property {string} userId - The unique identifier of the user.
+ * @property {string} campusId - The unique identifier of the campus.
+ * @property {string} [departmentId] - The unique identifier of the department.
+ * @property {boolean} isInstructor - Whether the staff is an instructor.
+ * @property {string} [instructorId] - The unique identifier of the instructor.
+ * @property {string} [specialty] - The specialty of the staff.
+ * @property {number} yearsOfService - The number of years the staff has been working.
+ * @property {string} [status] - The status of the staff.
+ * @property {Timestamp} [workStartAt] - The date and time the staff started working.
+ * @property {Timestamp} [workEndAt] - The date and time the staff ended working.
+ * @property {Timestamp} [lastChangeAt] - The date and time the staff was last changed.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @author Jarrian Vince Gojar
+ */
+export interface Staff {
+	staffId: string;
+	userId: string;
+	campusId: string;
+	departmentId?: string;
+	isInstructor: boolean;
+	instructorId?: string;
+	specialty?: string;
+	yearsOfService: number;
+	status?: "working" | "retired" | "quit";
+	workStartAt?: Timestamp;
+	workEndAt?: Timestamp;
+	lastChangeAt?: Timestamp;
+}

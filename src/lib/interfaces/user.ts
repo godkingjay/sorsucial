99e495eb53a051bbedc5c96e2163b8abeef50d31
Jarrian <glorianagojar@gmail.com>
2023-03-11 17:20:59 +0800

    fix:    Add optional fields to User interface

--- src/lib/interfaces/user.ts
+++ src/lib/interfaces/user.ts
@@ -15,14 +15,15 @@ import { GeoPoint, Timestamp } from "firebase/firestore";
  *
  * @property {string} uid - The unique identifier of the user.
  * @property {string} firstName - The first name of the user.
+ * @property {string} [middleName] - The middle name of the user.
  * @property {string} lastName - The last name of the user.
  * @property {string} email - The email address of the user.
- * @property {string} phoneNumber - The phone number of the user.
+ * @property {string} [phoneNumber] - The phone number of the user.
  * @property {boolean} isFirstLogin - Whether or not this is the first time the user has logged in.
  * @property {"admin" | "staff" | "student" | "instructor" | "user"} role - The role of the user.
- * @property {string} imageURL - The URL of the user's profile image.
+ * @property {string} [imageURL] - The URL of the user's profile image.
  * @property {Timestamp} [birthDate] - The date of birth of the user.
- * @property {"male" | "female" | "other"} gender - The gender of the user.
+ * @property {"male" | "female" | "other"} [gender] - The gender of the user.
  * @property {string} [currentStatusText] - The current status text of the user.
  * @property {string} [currentStatusEmoji] - The current status emoji of the user.
  * @property {number} numberOfConnections - The number of connections the user has.
@@ -32,7 +33,7 @@ import { GeoPoint, Timestamp } from "firebase/firestore";
  * @property {string} [stateOrProvince] - The state or province of the user.
  * @property {string} [cityOrMunicipality] - The city or municipality of the user.
  * @property {string} [streetAddress] - The street address of the user.
- * @property {Timestamp} [createdAt] - The date and time the user was created.
+ * @property {Timestamp} createdAt - The date and time the user was created.
  * @property {Timestamp} [previousNames] - The previous names of the user.
  * @property {Timestamp} [lastLoginAt] - The date and time the user last logged in.
  * @property {Timestamp} [lastChangeAt] - The date and time the user was last changed.
@@ -44,14 +45,15 @@ import { GeoPoint, Timestamp } from "firebase/firestore";
 export interface SiteUser {
 	uid: string;
 	firstName: string;
+	middleName?: string;
 	lastName: string;
 	email: string;
-	phoneNumber: string;
+	phoneNumber?: string;
 	isFirstLogin: boolean;
 	role: "admin" | "staff" | "student" | "instructor" | "user" | null;
-	imageURL: string;
+	imageURL?: string;
 	birthDate?: Timestamp;
-	gender: "male" | "female" | "other" | null;
+	gender?: "male" | "female" | "other";
 	currentStatusText?: string;
 	currentStatusEmoji?: string;
 	numberOfConnections: number;

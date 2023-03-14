f686a8c0ce0aed81494774fbbc91eef63b64a5ac
Jarrian <glorianagojar@gmail.com>
2023-03-14 21:53:40 +0800

    feat: Add directoryDropdown and menuDropdown states to NavigationBarAtom

--- src/atoms/navigationBarAtom.ts
+++ src/atoms/navigationBarAtom.ts
@@ -1,12 +1,24 @@
 import { atom } from "recoil";
 
 export interface NavigationBarState {
+	directoryDropdown: {
+		open: boolean;
+	};
+	menuDropdown: {
+		open: boolean;
+	};
 	userDropdown: {
 		open: boolean;
 	};
 }
 
 export const defaultNavigationBarState: NavigationBarState = {
+	directoryDropdown: {
+		open: false,
+	},
+	menuDropdown: {
+		open: false,
+	},
 	userDropdown: {
 		open: false,
 	},

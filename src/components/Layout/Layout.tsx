298d389cced3a201978f1baf7a278c6c7b9572a5
Jarrian <glorianagojar@gmail.com>
2023-03-14 19:22:48 +0800

    feat:    Update Layout and SignInPage components to include new elements and adjust existing elements

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -33,9 +33,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 						/>
 					)}
 					<div className="flex-1 flex flex-row">
-						<PageLeftSidebar />
-						<div className="flex-1 flex flex-row justify-center min-w-[256px]">
-							<div className="max-w-3xl w-full">{children}</div>
+						{authUser && !userStateValue.user.isFirstLogin && (
+							<PageLeftSidebar />
+						)}
+						<div className="flex-1 flex flex-row justify-center min-w-[256px] h-full">
+							<div className="max-w-3xl w-full h-full">{children}</div>
 						</div>
 					</div>
 				</>

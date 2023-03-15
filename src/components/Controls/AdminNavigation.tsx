97ea64c45a83dd4ba54a1fb318d912bc4faf713c
Jarrian <glorianagojar@gmail.com>
2023-03-15 18:12:34 +0800

    style: Refactor AdminNavigation component styling

--- src/components/Controls/AdminNavigation.tsx
+++ src/components/Controls/AdminNavigation.tsx
@@ -9,64 +9,70 @@ type AdminNavigationProps = {};
 const AdminNavigation: React.FC<AdminNavigationProps> = () => {
 	return (
 		<div className="sticky top-14">
-			<div className="h-14 w-full shadow-md bg-slate-700 overflow-x-auto scroll-x-style">
-				<ul className="h-full w-max flex flex-row items-center p-2 gap-x-1">
+			<div className="admin-nav-wrapper">
+				<ul className="admin-nav-container">
 					<li>
 						<button
 							type="button"
 							title="Dashboard"
-							className="flex flex-row items-center p-2 text-white gap-x-3 duration-200 hover:bg-slate-600 hover:rounded-md"
+							className="admin-nav-group"
 						>
-							<div className="h-6 w-6 aspect-square">
-								<MdSpaceDashboard className="h-full w-full" />
+							<div className="icon-container">
+								<MdSpaceDashboard className="icon" />
 							</div>
-							<div className="flex flex-1 flex-row h-full items-center">
-								<p className="font-semibold text-sm">Dashboard</p>
+							<div className="label-container">
+								<p className="label">Dashboard</p>
 							</div>
 						</button>
 					</li>
-					<li className="h-full">
-						<div className="h-full w-[1px] bg-white bg-opacity-20 mx-2"></div>
+					<li className="divider-container">
+						<div className="divider"></div>
 					</li>
 					<li>
 						<button
 							type="button"
 							title="Users"
-							className="flex flex-row items-center p-2 text-white gap-x-3 duration-200 hover:bg-slate-600 hover:rounded-md"
+							className="admin-nav-group"
 						>
-							<div className="h-6 w-6 aspect-square">
-								<FaUserCircle className="h-full w-full" />
+							<div className="icon-container">
+								<FaUserCircle className="icon" />
 							</div>
-							<div className="flex flex-1 flex-row h-full items-center">
-								<p className="font-semibold text-sm">Users</p>
+							<div className="label-container">
+								<p className="label">Users</p>
 							</div>
 						</button>
+					</li>
+					<li className="divider-container">
+						<div className="divider"></div>
 					</li>
 					<li>
 						<button
 							type="button"
 							title="Groups"
-							className="flex flex-row items-center p-2 text-white gap-x-3 duration-200 hover:bg-slate-600 hover:rounded-md"
+							className="admin-nav-group"
 						>
-							<div className="h-6 w-6 aspect-square">
-								<HiUserGroup className="h-full w-full" />
+							<div className="icon-container">
+								<HiUserGroup className="icon" />
 							</div>
-							<div className="flex flex-1 flex-row h-full items-center">
-								<p className="font-semibold text-sm">Groups</p>
+							<div className="label-container">
+								<p className="label">Groups</p>
 							</div>
 						</button>
+					</li>
+					<li className="divider-container">
+						<div className="divider"></div>
 					</li>
 					<li>
 						<button
 							type="button"
 							title="Requests"
-							className="flex flex-row items-center p-2 text-white gap-x-3 duration-200 hover:bg-slate-600 hover:rounded-md"
+							className="admin-nav-group"
 						>
-							<div className="h-6 w-6 aspect-square">
-								<GoRequestChanges className="h-full w-full" />
+							<div className="icon-container">
+								<GoRequestChanges className="icon" />
 							</div>
-							<div className="flex flex-1 flex-row h-full items-center">
-								<p className="font-semibold text-sm">Requests</p>
+							<div className="label-container">
+								<p className="label">Requests</p>
 							</div>
 						</button>
 					</li>

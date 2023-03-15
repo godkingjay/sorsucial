899e03fdce00bc0c7b4470477470005f0975b863
Jarrian <glorianagojar@gmail.com>
2023-03-15 19:48:53 +0800

    feat:    Add DiscussionsPage, FeedsPage, and GroupsPage components

---
+++ src/pages/discussions/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type DiscussionsPageProps = {};
+
+const DiscussionsPage: React.FC<DiscussionsPageProps> = () => {
+	return <div>DiscussionsPage</div>;
+};
+
+export default DiscussionsPage;

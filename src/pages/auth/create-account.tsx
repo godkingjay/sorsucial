5b385dfec3af8e64b0d0825ddcb67c8e7d390542
Jarrian <glorianagojar@gmail.com>
2023-03-16 21:15:16 +0800

    dox: create-account main page

--- src/pages/auth/create-account.tsx
+++ src/pages/auth/create-account.tsx
@@ -9,6 +9,15 @@ import React, { useEffect, useState } from "react";
 
 type CreateAccountPageProps = {};
 
+/**
+ * This is the type for the create account form.
+ *
+ * @typedef {Object} CreateAccountType - The type for the create account form.
+ *
+ * @property {string} email - The email of the user.
+ * @property {string} password - The password of the user.
+ * @property {string} repeatPassword - The repeated password of the user.
+ */
 export type CreateAccountType = {
 	email: string;
 	password: string;
@@ -16,8 +25,32 @@ export type CreateAccountType = {
 };
 
 const CreateAccountPage: React.FC<CreateAccountPageProps> = () => {
+	/**
+	 * Hooks for the create account page.
+	 *
+	 * @property {Object} authUser - The user object from the auth state.
+	 * @property {boolean} authLoading - The loading state of the auth state.
+	 * @property {Function} createAccount - The function to create an account.
+	 */
 	const { authUser, authLoading, createAccount } = useUser();
+
+	/**
+	 * State for the create account page.
+	 *
+	 * @property {boolean} loadingCreateAccount - The loading state of the create account page.
+	 */
 	const [loadingCreateAccount, setLoadingCreateAccount] = useState(false);
+
+	/**
+	 * State for the create account form.
+	 * The state is initialized with an empty object.
+	 * The state changes when the user types in the input fields.
+	 *
+	 * @property {Object} createAccountForm - The state for the create account form.
+	 * @property {Function} setCreateAccountForm - The function to set the state of the create account form.
+	 *
+	 * @see {@link CreateAccountType} for the type of the create account form.
+	 */
 	const [createAccountForm, setCreateAccountForm] = useState<CreateAccountType>(
 		{
 			email: "",
@@ -26,8 +59,15 @@ const CreateAccountPage: React.FC<CreateAccountPageProps> = () => {
 		}
 	);
 
+	/**
+	 * Router for the create account page.
+	 * The router is used to redirect the user to the home page if the user is already logged in.
+	 */
 	const router = useRouter();
 
+	/**
+	 * This function initializes the create account form.
+	 */
 	const initializeCreateAccount = async () => {
 		setCreateAccountForm((prev) => ({
 			...prev,
@@ -35,6 +75,13 @@ const CreateAccountPage: React.FC<CreateAccountPageProps> = () => {
 		}));
 	};
 
+	/**
+	 * This effect initializes the create account form.
+	 * Changes the state of the create account page to loading.
+	 *
+	 * @see {@link initializeCreateAccount} for the function that initializes the create account form.
+	 * @listens {Object} router - The router object.
+	 */
 	useEffect(() => {
 		setLoadingCreateAccount(false);
 		if (
@@ -48,6 +95,12 @@ const CreateAccountPage: React.FC<CreateAccountPageProps> = () => {
 		}
 	}, [router]);
 
+	/**
+	 * This effect redirects the user to the home page if the user is already logged in.
+	 *
+	 * @see {@link authUser} for the user object from the auth state.
+	 * @see {@link authLoading} for the loading state of the auth state.
+	 */
 	useEffect(() => {
 		if (authUser && !authLoading) {
 			router.push("/");

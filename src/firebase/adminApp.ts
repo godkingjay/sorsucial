19e7f3ac89f8dadb7fd5b0c759d675edc27fe369
Jarrian <glorianagojar@gmail.com>
2023-03-17 17:13:04 +0800

    feat:    Add database URL and authentication admin services to adminApp

--- src/firebase/adminApp.ts
+++ src/firebase/adminApp.ts
@@ -7,7 +7,12 @@ const adminApp = initializeApp({
 		privateKey: process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY,
 	}),
 	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+	databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
 	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
 });
 
-export { adminApp };
+const authAdmin = adminApp.auth();
+const firestoreAdmin = adminApp.firestore();
+const storageAdmin = adminApp.storage();
+
+export { adminApp, authAdmin, firestoreAdmin, storageAdmin };
--- src/firebase/clientApp.ts
+++ src/firebase/clientApp.ts
@@ -5,7 +5,7 @@ import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
 	apiKey: process.env.NEXT_PUBLIC_API_KEY,
-	// databaseUrl: process.env.NEXT_PUBLIC_DATABASE_URL,
+	databaseUrl: process.env.NEXT_PUBLIC_DATABASE_URL,
 	authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
 	projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
 	storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,

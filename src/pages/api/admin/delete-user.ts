b3282d847e617c8df1124a7b3c07b166425bf2d7
Jarrian <glorianagojar@gmail.com>
2023-03-18 22:08:07 +0800

    feat:    Add delete-user API endpoint for admin users

---
+++ src/pages/api/admin/delete-user.ts
@@ -0,0 +1,49 @@
+import { authAdmin } from "@/firebase/adminApp";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	if (!req.body) {
+		res.status(400).json({ message: "Bad request" });
+		return;
+	}
+
+	const { uid, privateKey } = req.body;
+
+	if (!uid) {
+		res.status(400).json({ message: "UID is required" });
+		return;
+	}
+
+	if (
+		!privateKey ||
+		privateKey !==
+			process.env.NEXT_PUBLIC_ADMIN_PRIVATE_KEY?.replace(/\\n/g, "\n")
+	) {
+		res.status(401).json({ message: "Unauthorized" });
+		return;
+	}
+
+	if (req.method === "POST") {
+		try {
+			authAdmin
+				.deleteUser(uid)
+				.then(() => {
+					res
+						.status(200)
+						.json({ message: "User deleted successfully", isDeleted: true });
+				})
+				.catch((error) => {
+					console.log(`Admin deleting user ${uid}: `, error.message);
+					throw error;
+				});
+		} catch (error) {
+			console.error(error);
+			res
+				.status(500)
+				.json({ message: "Error deleting user", isDeleted: false });
+		}
+	} else {
+		res.status(405).json({ message: "Method not allowed" });
+		return;
+	}
+}

9532d645bacf2586c12c0a7d2d9cc3ef8fe227be
Jarrian <glorianagojar@gmail.com>
2023-03-19 15:23:28 +0800

    feat: Add PostCreationListener component to Home page for admins

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,22 @@
+import PostCreationListener from "@/components/Post/PostCreationListener";
+import useUser from "@/hooks/useUser";
+import Image from "next/image";
+import { FaUserCircle } from "react-icons/fa";
+
 export default function Home() {
+	const { userStateValue } = useUser();
+
 	return (
 		<>
-			<main>
-				<p>Test</p>
+			<main className="flex flex-col w-full py-4 px-2">
+				<section className="flex flex-col gap-y-4">
+					{userStateValue.user.roles.includes("admin") && (
+						<PostCreationListener
+							useStateValue={userStateValue}
+							postType="announcement"
+						/>
+					)}
+				</section>
 			</main>
 		</>
 	);

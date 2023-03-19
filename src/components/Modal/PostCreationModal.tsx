9cd243b37ec1d15954e3175741829144f3805592
Jarrian <glorianagojar@gmail.com>
2023-03-19 16:42:40 +0800

    feat:    Update modal title to dynamically display post type

--- src/components/Modal/PostCreationModal.tsx
+++ src/components/Modal/PostCreationModal.tsx
@@ -25,7 +25,13 @@ const PostCreationModal: React.FC<PostCreationModalProps> = ({
 		<div className="fixed w-full h-full bg-black bg-opacity-25 z-[1000] flex flex-col items-center px-8 py-16 overflow-y-auto scroll-y-style overflow-x-hidden">
 			<div className="w-full max-w-xl bg-white flex flex-col rounded-xl shadow-around-lg pointer-events-auto">
 				<div className="p-4 px-16 w-full rounded-t-xl flex flex-row justify-center items-center relative">
-					<p className="font-bold text-lg text-black truncate">Create a Post</p>
+					<p className="font-bold text-lg text-black truncate">
+						{postCreationModalStateValue.postType === "announcement" &&
+							"Create Announcement"}
+						{postCreationModalStateValue.postType === "feed" && "Create Post"}
+						{postCreationModalStateValue.postType === "group" &&
+							"Create Group Post"}
+					</p>
 					<button
 						type="button"
 						title="Close"
@@ -36,7 +42,10 @@ const PostCreationModal: React.FC<PostCreationModalProps> = ({
 					</button>
 				</div>
 				<div className="h-[1px] bg-black bg-opacity-10"></div>
-				<div className="p-4 flex flex-col"></div>
+				<div className="p-4 flex flex-col">
+					{/* <div className="w-full flex flex-col flex-1 items-center h-14 overflow-x-auto scroll-x-style"></div> */}
+					<div className="bg-white p-4 shadow-around-sm border border-gray-200 rounded-lg"></div>
+				</div>
 			</div>
 		</div>
 	);

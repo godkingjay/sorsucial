6dd3fee894260339090c9e5ed751947a51819229
Jarrian <glorianagojar@gmail.com>
2023-03-19 15:25:57 +0800

    feat:    Add PostCreationListener component on Feeds page

--- src/pages/feeds/index.tsx
+++ src/pages/feeds/index.tsx
@@ -1,9 +1,24 @@
+import PostCreationListener from "@/components/Post/PostCreationListener";
+import useUser from "@/hooks/useUser";
 import React from "react";
 
 type FeedsPageProps = {};
 
 const FeedsPage: React.FC<FeedsPageProps> = () => {
-	return <div>FeedsPage</div>;
+	const { userStateValue } = useUser();
+
+	return (
+		<main className="flex flex-col w-full py-4 px-2">
+			<section className="flex flex-col gap-y-4">
+				{userStateValue.user.roles.includes("user") && (
+					<PostCreationListener
+						useStateValue={userStateValue}
+						postType="feed"
+					/>
+				)}
+			</section>
+		</main>
+	);
 };
 
 export default FeedsPage;

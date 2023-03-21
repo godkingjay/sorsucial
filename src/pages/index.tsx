65259c65b9c6e0f169d393b94e32b1431cc480fb
Jarrian <glorianagojar@gmail.com>
2023-03-21 09:49:58 +0800

    feat: get announcements

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,31 @@
+import { userState } from "@/atoms/userAtom";
 import PostCreationListener from "@/components/Post/PostCreationListener";
+import usePost from "@/hooks/usePost";
 import useUser from "@/hooks/useUser";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
 	const { userStateValue } = useUser();
+	const { postStateValue, setPostStateValue, fetchPosts } = usePost();
+	const [loadingAnnouncements, setLoadingAnnouncements] = useState(true);
+	const anouncementsMounted = useRef(false);
+
+	const fetchAnnouncements = async () => {
+		setLoadingAnnouncements(true);
+		try {
+			await fetchPosts("announcement");
+		} catch (error: any) {
+			console.log("Hook: fetching announcement Error: ", error.message);
+		}
+		setLoadingAnnouncements(false);
+	};
+
+	useEffect(() => {
+		if (!anouncementsMounted.current && postStateValue.posts.length === 0) {
+			anouncementsMounted.current = true;
+			fetchAnnouncements();
+		}
+	}, []);
 
 	return (
 		<>
@@ -14,6 +37,11 @@ export default function Home() {
 							postType="announcement"
 						/>
 					)}
+					{postStateValue.posts
+						.filter((post) => post.postType === "announcement")
+						.map((post) => (
+							<div key={post.id}>{post.postTitle}</div>
+						))}
 				</section>
 			</main>
 		</>

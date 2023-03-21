837c2a6e81f1899f9c8ff4241f2ba23f72a7fe4a
Jarrian <glorianagojar@gmail.com>
2023-03-21 10:00:10 +0800

    feat: post card

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,11 @@ import { userState } from "@/atoms/userAtom";
 import PostCreationListener from "@/components/Post/PostCreationListener";
 import usePost from "@/hooks/usePost";
 import useUser from "@/hooks/useUser";
+import moment from "moment";
+import Image from "next/image";
+import Link from "next/link";
 import { useEffect, useRef, useState } from "react";
+import { FaUserCircle } from "react-icons/fa";
 
 export default function Home() {
 	const { userStateValue } = useUser();
@@ -40,7 +44,39 @@ export default function Home() {
 					{postStateValue.posts
 						.filter((post) => post.postType === "announcement")
 						.map((post) => (
-							<div key={post.id}>{post.postTitle}</div>
+							<div
+								key={post.id}
+								className="break-words flex flex-col shadow-page-box-1 bg-white rounded-lg"
+							>
+								<div className="p-2 flex flex-row h-14 items-center gap-x-4">
+									<Link
+										href={`/user/${userStateValue.user.uid}`}
+										className="h-10 w-10 aspect-square rounded-full border border-transparent text-gray-300"
+									>
+										{userStateValue.user.imageURL ? (
+											<Image
+												src={userStateValue.user.imageURL}
+												alt="User Profile Picture"
+												width={96}
+												height={96}
+												loading="lazy"
+												className="h-full w-full"
+											/>
+										) : (
+											<FaUserCircle className="h-full w-full bg-white" />
+										)}
+									</Link>
+									<div className="flex-1 flex flex-col h-full">
+										<p className="text-sm font-bold">{`${post.creatorId}`}</p>
+										<p className="text-2xs text-gray-500">
+											{moment(
+												new Date(post.createdAt.seconds * 1000)
+											).fromNow()}
+										</p>
+									</div>
+								</div>
+								<p>{JSON.stringify(post)}</p>
+							</div>
 						))}
 				</section>
 			</main>

20355e2f1d16fdc724347ceae64fc263efdc7da7
Jarrian <glorianagojar@gmail.com>
2023-03-21 13:52:19 +0800

    doc: postAtom

--- src/atoms/postAtom.ts
+++ src/atoms/postAtom.ts
@@ -10,16 +10,71 @@ import {
 import { SiteUser } from "@/lib/interfaces/user";
 import { atom } from "recoil";
 
+/**
+ * This is the data that is needed to display a poll item in the UI.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @interface PollItemData
+ * @see {@link PollItemData}
+ *
+ * ----------------------------------------------------------------
+ *
+ * @property {PollItem} pollItem - The poll item data.
+ * @property {PollItemLogo} pollItemLogo - The poll item logo data.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @author Jarrian Vince Gojar
+ */
 export interface PollItemData {
 	pollItem: PollItem;
 	pollItemLogo: PollItemLogo;
 }
 
+/**
+ * This is the data that is needed to display a post poll in the UI.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @interface PostPollData
+ * @see {@link PostPollData}
+ *
+ * ----------------------------------------------------------------
+ *
+ * @property {PostPoll} poll - The post poll data.
+ * @property {PollItemData[]} pollItems - The poll item data.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @author Jarrian Vince Gojar
+ */
 export interface PostPollData {
 	poll: PostPoll;
 	pollItems: PollItemData[];
 }
 
+/**
+ * This is the data that is needed to display a post in the UI.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @interface PostData
+ * @see {@link PostData}
+ *
+ * ----------------------------------------------------------------
+ *
+ * @property {SitePost} post - The post data.
+ * @property {SiteUser | null} creator - The post creator data.
+ * @property {PostImageOrVideo[]} postImagesOrVideos - The post images or videos data.
+ * @property {PostFile[]} postFiles - The post files data.
+ * @property {PostLink[]} postLinks - The post links data.
+ * @property {PostPollData} postPoll - The post poll data.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @author Jarrian Vince Gojar
+ */
 export interface PostData {
 	post: SitePost;
 	creator: SiteUser | null;
@@ -29,16 +84,38 @@ export interface PostData {
 	postPoll?: PostPollData;
 }
 
+/**
+ * This is the state of the post atom.
+ *
+ * ----------------------------------------------------------------
+ *
+ * @interface PostState
+ * @see {@link PostState}
+ *
+ * ----------------------------------------------------------------
+ *
+ * @author Jarrian Vince Gojar
+ */
 export interface PostState {
 	posts: PostData[];
 	currentPost: SitePost | null;
 }
 
+/**
+ * This is the default state of the post atom.
+ */
 export const defaultPostState: PostState = {
 	posts: [],
 	currentPost: null,
 };
 
+/**
+ * This is the post atom.
+ *
+ * @see {@link defaultPostState} - default state of the post atom.
+ * @see {@link PostState} - the data structure of the post atom
+ * @see {@link PostData} - the data structure of the post data in the post atom state.
+ */
 export const postState = atom<PostState>({
 	key: "postState",
 	default: defaultPostState,

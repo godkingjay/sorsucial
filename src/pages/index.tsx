fa753db9f693d7f647b957c9c0556ba1a1f7b2d7
Jarrian <glorianagojar@gmail.com>
2023-03-21 20:33:17 +0800

    feat:    Update useUser to include authUser, authLoading, and loadingUser in Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import useUser from "@/hooks/useUser";
 import { useCallback, useEffect, useRef, useState } from "react";
 
 export default function Home() {
-	const { userStateValue } = useUser();
+	const { userStateValue, authUser, authLoading, loadingUser } = useUser();
 	const { postStateValue, setPostStateValue, fetchPosts } = usePost();
 	const [loadingAnnouncements, setLoadingAnnouncements] = useState(true);
 	const anouncementsMounted = useRef(false);
@@ -18,14 +18,27 @@ export default function Home() {
 			console.log("Hook: fetching announcement Error: ", error.message);
 		}
 		setLoadingAnnouncements(false);
-	}, []);
+	}, [fetchPosts]);
 
 	useEffect(() => {
-		if (!anouncementsMounted.current && postStateValue.posts.length === 0) {
+		if (
+			!anouncementsMounted.current &&
+			postStateValue.posts.length === 0 &&
+			userStateValue.user &&
+			authUser &&
+			!loadingUser &&
+			!authLoading
+		) {
 			anouncementsMounted.current = true;
 			fetchAnnouncements();
 		}
-	}, [anouncementsMounted, fetchAnnouncements, postStateValue.posts]);
+	}, [
+		anouncementsMounted,
+		fetchAnnouncements,
+		postStateValue.posts,
+		authUser,
+		userStateValue.user,
+	]);
 
 	return (
 		<>

edae4dc0555af4dadd980a8d5612d9a5b469a1ec
Jarrian <glorianagojar@gmail.com>
2023-03-21 13:44:45 +0800

    feat[Atom]:add properties to PostData

--- src/atoms/postAtom.ts
+++ src/atoms/postAtom.ts
@@ -1,10 +1,32 @@
-import { SitePost } from "@/lib/interfaces/post";
+import {
+	PollItem,
+	PollItemLogo,
+	PostFile,
+	PostImageOrVideo,
+	PostLink,
+	PostPoll,
+	SitePost,
+} from "@/lib/interfaces/post";
 import { SiteUser } from "@/lib/interfaces/user";
 import { atom } from "recoil";
+
+export interface PollItemData {
+	pollItem: PollItem;
+	pollItemLogo: PollItemLogo;
+}
+
+export interface PostPollData {
+	poll: PostPoll;
+	pollItems: PollItemData[];
+}
 
 export interface PostData {
 	post: SitePost;
 	creator: SiteUser | null;
+	postImagesOrVideos?: PostImageOrVideo[];
+	postFiles?: PostFile[];
+	postLinks?: PostLink[];
+	postPoll?: PostPollData;
 }
 
 export interface PostState {

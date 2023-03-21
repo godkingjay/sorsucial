8d29375bb4452c6fedf71e405fd3e81a36a0e888
Jarrian <glorianagojar@gmail.com>
2023-03-21 18:36:22 +0800

    feat: break-words

--- src/components/Post/PostCard.tsx
+++ src/components/Post/PostCard.tsx
@@ -31,7 +31,7 @@ const PostCard: React.FC<PostCardProps> = ({ userStateValue, postData }) => {
 	};
 
 	return (
-		<div className="break-words flex flex-col shadow-page-box-1 bg-white rounded-lg">
+		<div className="flex flex-col shadow-page-box-1 bg-white rounded-lg">
 			<div className="p-4 flex flex-row h-18 items-center gap-x-4">
 				<Link
 					href={`/user/${userStateValue.user.uid}`}
@@ -58,21 +58,23 @@ const PostCard: React.FC<PostCardProps> = ({ userStateValue, postData }) => {
 				</div>
 			</div>
 			<div className="flex flex-col px-4 pb-4 gap-y-2">
-				<h1 className="text-lg font-bold">{postData.post.postTitle}</h1>
+				<h1 className="text-lg font-bold whitespace-pre-wrap break-words w-full">
+					{postData.post.postTitle}
+				</h1>
 				{postData.post.postBody && (
 					<>
 						<div className="h-[1px] bg-black bg-opacity-10"></div>
-						<div className="flex flex-col items-start">
-							<p className="text-sm text-justify">
+						<div className="flex flex-col items-start break-words">
+							<p className="text-sm text-justify whitespace-pre-wrap break-words w-full">
 								{postBody}
 								{postData.post.postBody.length > 256 && (
 									<>
-										{"     "}
+										{"\n"}
 										<button
 											type="button"
 											title={seeMore ? "See Less" : "See More"}
 											onClick={handleSeeMore}
-											className="inline-block text-gray-400"
+											className="text-gray-400 text-xs hover:text-gray-500"
 										>
 											{seeMore ? "...See Less" : "See More..."}
 										</button>

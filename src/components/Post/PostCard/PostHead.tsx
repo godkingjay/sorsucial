dedf500dbffdcb1d33f1bcd6df673e5c4b209401
Jarrian <glorianagojar@gmail.com>
2023-03-27 17:45:36 +0800

    feat:    Update PostHead to render different profile images for announcements and users

--- src/components/Post/PostCard/PostHead.tsx
+++ src/components/Post/PostCard/PostHead.tsx
@@ -24,29 +24,54 @@ const PostHead: React.FC<PostHeadProps> = ({
 }) => {
 	return (
 		<div className="p-4 flex flex-row h-18 items-center gap-x-4 relative">
-			<Link
-				href={`/user/${postData.creator?.uid}`}
-				className="h-10 w-10 aspect-square rounded-full overflow-hidden border border-transparent text-gray-300"
-			>
-				{postData.creator?.imageURL ? (
+			{postData.post.postType === "announcement" ? (
+				<Link
+					href={`/user/sorsu`}
+					className="h-10 w-10 aspect-square rounded-full overflow-hidden border border-transparent text-gray-300"
+				>
 					<Image
-						src={postData.creator?.imageURL}
+						src={"/assets/logo/sorsu-sm.png"}
 						alt="User Profile Picture"
-						width={96}
-						height={96}
+						width={128}
+						height={128}
 						loading="lazy"
 						className="h-full w-full"
 					/>
-				) : (
-					<FaUserCircle className="h-full w-full bg-white" />
-				)}
-			</Link>
+				</Link>
+			) : (
+				<Link
+					href={`/user/${postData.creator?.uid}`}
+					className="h-10 w-10 aspect-square rounded-full overflow-hidden border border-transparent text-gray-300"
+				>
+					{postData.creator?.imageURL ? (
+						<Image
+							src={postData.creator?.imageURL}
+							alt="User Profile Picture"
+							width={96}
+							height={96}
+							loading="lazy"
+							className="h-full w-full"
+						/>
+					) : (
+						<FaUserCircle className="h-full w-full bg-white" />
+					)}
+				</Link>
+			)}
 			<div className="flex-1 flex flex-col h-full pr-6">
 				<div className="w-full truncate">
-					<Link
-						href={`/user/${postData.creator?.uid}`}
-						className="truncate font-semibold hover:underline focus:underline"
-					>{`${postData.creator?.firstName} ${postData.creator?.lastName}`}</Link>
+					{postData.post.postType === "announcement" ? (
+						<Link
+							href={`/user/sorsu`}
+							className="truncate font-semibold hover:underline focus:underline"
+						>
+							Sorsogon State University
+						</Link>
+					) : (
+						<Link
+							href={`/user/${postData.creator?.uid}`}
+							className="truncate font-semibold hover:underline focus:underline"
+						>{`${postData.creator?.firstName} ${postData.creator?.lastName}`}</Link>
+					)}
 				</div>
 				<div className="flex flex-row items-center truncate">
 					<p className="text-2xs text-gray-500 truncate">

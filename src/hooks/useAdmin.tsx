482b8c3552b1ccb298ba9e84dd6686e63c8afffd
Jarrian <glorianagojar@gmail.com>
2023-03-31 16:03:41 +0800

    doc: create new USers

--- src/hooks/useAdmin.tsx
+++ src/hooks/useAdmin.tsx
@@ -6,21 +6,106 @@ import { SiteUser } from "@/lib/interfaces/user";
 import axios from "axios";
 import { useRecoilState } from "recoil";
 
+/**
+ * useAdmin
+ *
+ * This hook is used to manage the admin state and admin modal state.
+ *
+ * @returns {object} - {adminStateValue, setAdminStateValue, adminModalStateValue, setAdminModalStateValue, createNewUsers, deleteUser}
+ */
 const useAdmin = () => {
+	/**
+	 * adminStateValue - The current value of the admin state.
+	 *
+	 * setAdminStateValue - The function used to set the admin state.
+	 *
+	 * @param {object} - {manageUsers: SiteUser[], manageGroups: SiteGroup[], manageDiscussions: SiteDiscussion[], manageFeeds: SiteFeed[]}
+	 *
+	 * manageUsers - The list of users to be managed.
+	 *
+	 * manageGroups - The list of groups to be managed.
+	 *
+	 * manageDiscussions - The list of discussions to be managed.
+	 *
+	 * manageFeeds - The list of feeds to be managed.
+	 */
 	const [adminStateValue, setAdminStateValue] = useRecoilState(adminState);
+	/**
+	 * adminModalStateValue - The current value of the admin modal state.
+	 *
+	 * setAdminModalStateValue - The function used to set the admin modal state.
+	 *
+	 * @param {object} - {isOpen: boolean, modalType: string, modalProps: object}
+	 *
+	 * isOpen - The boolean value that determines if the modal is open or not.
+	 *
+	 * modalType - The type of modal to be opened.
+	 *
+	 * modalProps - The props to be passed to the modal.
+	 */
 	const [adminModalStateValue, setAdminModalStateValue] =
 		useRecoilState(adminModalState);
 
+	/**
+	 * createNewUsers
+	 *
+	 * This function is used to create new users.
+	 *
+	 * This function will create a new user in the admin state and in the database.
+	 * It will send a request to the API to create a new account in firebase account.
+	 * After creating an account in firebase, it will send another request to the API to create a new user document in the database.
+	 * After creating a new user, it will then add the new user to the admin state.
+	 *
+	 * @param {NewUserType[]} newUsers - The list of new users to be created.
+	 */
 	const createNewUsers = async (newUsers: NewUserType[]) => {
 		try {
+			/**
+			 * newAdminStateUsers - The list of new users to be added to the admin state.
+			 *
+			 * This list will be used to add the new users to the admin state.
+			 *
+			 * @type {SiteUser[]}
+			 */
 			const newAdminStateUsers: SiteUser[] = [];
 
+			/**
+			 * This will loop through the list of new users and create a new user in the database.
+			 * This will create a new user in the database and add the new user to the admin state.
+			 */
 			await Promise.all(
+				/**
+				 * This will loop through the list of new users.
+				 */
 				newUsers.map(async (newUser) => {
+					/**
+					 * This will try to create a new user in the database.
+					 * This will send a request to the API to create a new account in firebase account.
+					 * This will send another request to the API to create a new user document in the database.
+					 * This will then add the new user to the admin state.
+					 *
+					 * If an error occurs, it will catch the error and log it to the console.
+					 */
 					try {
+						/**
+						 * email - The email of the new user.
+						 * password - The password of the new user.
+						 */
 						const { email, password } = newUser;
 
+						/**
+						 * This will try to create a new user in the database.
+						 * This will send a request to the API to create a new account in firebase account.
+						 * This will send another request to the API to create a new user document in the database.
+						 * This will then add the new user to the admin state.
+						 *
+						 * If an error occurs, it will catch the error and log it to the console.
+						 */
 						try {
+							/**
+							 * This will send a request to the API to create a new account in firebase account.
+							 * If the request is successful, it will get the user id of the new user returned by the request sent.
+							 */
 							await axios
 								.post(apiConfig.apiEndpoint + "admin/manage/account/account", {
 									newUserEmail: email,
@@ -28,11 +113,27 @@ const useAdmin = () => {
 									postPrivateKey: apiConfig.privateKey,
 								})
 								.then(async (res) => {
+									/**
+									 * userId - The user id of the new user.
+									 */
 									const { userId } = res.data;
 
+									/**
+									 * This date will be used to set the createdAt and updatedAt fields of the new user.
+									 */
 									const date = new Date();
 
+									/**
+									 * If a user id is returned by the request sent, it will create a new user document in the database.
+									 *
+									 * If a user id is not returned by the request sent, it will log an error to the console.
+									 */
 									if (userId) {
+										/**
+										 * newUserDoc - The new user document to be created in the database.
+										 *
+										 * This will be used to create a new user document in the database.
+										 */
 										const newUserDoc: SiteUser = {
 											uid: userId,
 											email: newUser.email,
@@ -54,6 +155,13 @@ const useAdmin = () => {
 											updatedAt: date,
 										};
 
+										/**
+										 * This will send a request to the API to create a new user document in the database.
+										 *
+										 * If the request is successful, it will get the new user returned by the request sent.
+										 *
+										 * If the request is not successful, it will log an error to the console.
+										 */
 										const newUserData = await axios
 											.post(apiConfig.apiEndpoint + "user/user", {
 												newUser: newUserDoc,
@@ -67,9 +175,17 @@ const useAdmin = () => {
 												);
 											});
 
+										/**
+										 * This will add the new user to the list of new users.
+										 */
 										newAdminStateUsers.push({
 											...newUserData,
 										} as SiteUser);
+									} else {
+										console.log("API: Creating Account Error: ", newUser.email);
+										throw new Error(
+											"API: Creating Account Error: " + newUser.email
+										);
 									}
 								})
 								.catch((error: any) => {
@@ -84,6 +200,9 @@ const useAdmin = () => {
 				})
 			);
 
+			/**
+			 * This will add the new users to the admin state.
+			 */
 			setAdminStateValue((prev) => ({
 				...prev,
 				manageUsers: [...newAdminStateUsers, ...prev.manageUsers],

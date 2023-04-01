2c9edacac48b65ebca19f791f2673497067f5d59
Jarrian <glorianagojar@gmail.com>
2023-04-01 19:02:52 +0800

    feat: add PostFileCard Component in Post Card

--- src/components/Post/PostCard/PostBody/PostFiles.tsx
+++ src/components/Post/PostCard/PostBody/PostFiles.tsx
@@ -3,6 +3,7 @@ import FileIcons from "@/components/Icons/FileIcons";
 import { validAllTypes } from "@/lib/types/validFiles";
 import React from "react";
 import { HiDownload, HiOutlineDownload } from "react-icons/hi";
+import PostFileCard from "./PostCards/PostFileCard";
 
 type PostFilesProps = {
 	postData: PostData;
@@ -23,45 +24,12 @@ const PostFiles: React.FC<PostFilesProps> = ({ postData, formatFileSize }) => {
 						null;
 
 					return (
-						<div
-							className="post-file-item"
+						<PostFileCard
 							key={file.id}
-							file-type={fileDetails?.type || "file"}
-						>
-							<div className="deco-1"></div>
-							<div className="logo-container">
-								<FileIcons type={fileDetails ? fileDetails.type : ""} />
-							</div>
-							<div className="details-container">
-								<div className="details-header">
-									<h2 className="file-name">
-										{file.fileTitle || file.fileName}
-									</h2>
-									<p className="extra">
-										<span className="name">{file.fileName}</span>
-										<span className="file-extra">
-											<span className="size">
-												{formatFileSize(file.fileSize)}
-											</span>
-											<span>•</span>
-											<span className="extension">{file.fileExtension}</span>
-										</span>
-									</p>
-								</div>
-								<div className="buttons-container">
-									<a
-										download={file.fileName}
-										href={file.fileUrl}
-										target="_blank"
-										title="Download"
-										className="button download"
-										rel="noopener"
-									>
-										<HiOutlineDownload className="icon" />
-									</a>
-								</div>
-							</div>
-						</div>
+							file={file}
+							fileDetails={fileDetails}
+							formatFileSize={formatFileSize}
+						/>
 					);
 				})}
 			</div>

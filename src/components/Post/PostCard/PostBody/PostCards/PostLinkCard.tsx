0f71fbe73e3de9ff8a08fe7c7934a1e81cbca507
Jarrian <glorianagojar@gmail.com>
2023-04-01 18:28:05 +0800

    style:    Refactor PostLinkCard styles to use classes

--- src/components/Post/PostCard/PostBody/PostCards/PostLinkCard.tsx
+++ src/components/Post/PostCard/PostBody/PostCards/PostLinkCard.tsx
@@ -11,19 +11,15 @@ const PostLinkCard: React.FC<PostLinkCardProps> = ({ postLink }) => {
 	return (
 		<a
 			href={postLink.url}
-			className="flex flex-row bg-white p-2 shadow-page-box-1 rounded-lg relative gap-x-2"
+			className="post-link-card"
 		>
-			<div className="h-16 w-16 rounded-lg border border-gray-500 border-opacity-25 p-3 text-gray-400 bg-gray-50">
-				<SlGlobe className="h-full w-full" />
+			<div className="post-link-logo-container">
+				<SlGlobe className="post-link-logo" />
 			</div>
-			<div className="flex flex-col flex-1 gap-y-1">
-				<div className="flex flex-col">
-					<h1 className="font-bold truncate">
-						{new URL(postLink.url).hostname}
-					</h1>
-					<h2 className="text-xs truncate underline text-gray-400">
-						{postLink.url}
-					</h2>
+			<div className="post-link-details-container">
+				<div className="post-link-header-container">
+					<h1 className="post-link-title">{new URL(postLink.url).hostname}</h1>
+					<h2 className="post-link-full-url">{postLink.url}</h2>
 				</div>
 			</div>
 		</a>

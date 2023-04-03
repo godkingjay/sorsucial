ab543db1c6169a91338516e689d7bdda5787f0ba
Jarrian <glorianagojar@gmail.com>
2023-04-03 08:39:51 +0800

    chore: add meta for whole site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,7 +11,13 @@ export default function App({ Component, pageProps }: AppProps) {
 				<title>SorSUcial</title>
 				<meta
 					name="description"
+					property="og:description"
 					content="SorSUcial is the unofficial social media for the University of Sorsogon State University."
+				/>
+				<meta
+					name="site_name"
+					property="og:site_name"
+					content="SorSUcial"
 				/>
 				<link
 					rel="icon"

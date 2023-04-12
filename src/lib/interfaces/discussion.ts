8181d891772e23b24efe03c1da81cfa8eff4c14f
Jarrian <glorianagojar@gmail.com>
2023-04-12 20:46:38 +0800

    fix: letter casing for creatorId

--- src/lib/interfaces/discussion.ts
+++ src/lib/interfaces/discussion.ts
@@ -99,7 +99,7 @@ export interface DiscussionVote {
  * ----------------------------------------------------------------
  *
  * @property {string} id - The unique identifier of the reply.
- * @property {string} creatorID - The unique identifier of the reply creator.
+ * @property {string} creatorId - The unique identifier of the reply creator.
  * @property {string} discussionId - The unique identifier of the discussion.
  * @property {string} replyForId - The unique identifier of the reply.
  * @property {string} [groupId] - The unique identifier of the group.
@@ -118,7 +118,7 @@ export interface DiscussionVote {
  */
 export interface Reply {
 	id: string;
-	creatorID: string;
+	creatorId: string;
 	discussionId: string;
 	replyForId: string;
 	groupId?: string;

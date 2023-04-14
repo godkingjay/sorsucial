835e7662fc40fb3f33a016665432c01ccf444602
Jarrian <glorianagojar@gmail.com>
2023-04-14 10:21:46 +0800

    feat: Add loading state and discussion page to Discussions page

--- src/pages/discussions/index.tsx
+++ src/pages/discussions/index.tsx
@@ -1,16 +1,32 @@
+import LimitedBodyLayout from "@/components/Layout/LimitedBodyLayout";
+import useUser from "@/hooks/useUser";
 import Head from "next/head";
-import React from "react";
+import React, { useRef, useState } from "react";
 
 type DiscussionsPageProps = {};
 
 const DiscussionsPage: React.FC<DiscussionsPageProps> = () => {
+	const { userMounted } = useUser();
+	const [firstLoadingDiscussions, setFirstLoadingDiscussions] = useState(false);
+	const discussionsMounted = useRef(false);
+
 	return (
 		<>
 			<Head>
 				<title>Discussions | SorSUcial</title>
 			</Head>
-			<main>
-				<p>DiscussionsPage</p>
+			<main className="flex flex-col flex-1 py-4 px-4">
+				<LimitedBodyLayout>
+					<section className="flex flex-col gap-y-4">
+						{firstLoadingDiscussions && !userMounted ? (
+							<>
+								<p>Loading</p>
+							</>
+						) : (
+							<div>Discussion</div>
+						)}
+					</section>
+				</LimitedBodyLayout>
 			</main>
 		</>
 	);

8462420a7d63cb7d1610ca20d76e20c3742ad479
Jarrian <glorianagojar@gmail.com>
2023-04-16 10:09:06 +0800

    feat:    Add DiscussionShareMenu component and styles

--- src/components/Discussion/DiscussionCard/DiscussionFooter.tsx
+++ src/components/Discussion/DiscussionCard/DiscussionFooter.tsx
@@ -1,13 +1,25 @@
-import { DiscussionData } from "@/atoms/discussionAtom";
+import { DiscussionData, DiscussionOptionsState } from "@/atoms/discussionAtom";
 import React from "react";
+import { BsFacebook } from "react-icons/bs";
 import { GoCommentDiscussion } from "react-icons/go";
+import { IoLinkOutline } from "react-icons/io5";
 import { RiShareForwardLine } from "react-icons/ri";
+import { discussionShareType } from "../DiscussionCard";
+import DiscussionShareMenu from "./DiscussionFooter/DiscussionShareMenu";
 
 type DiscussionFooterProps = {
 	discussionData: DiscussionData;
+	discussionOptionsStateValue: DiscussionOptionsState;
+	handleDiscussionOptions: (name: keyof DiscussionOptionsState) => void;
+	handleFooterShareClick: (type: discussionShareType) => void;
 };
 
-const DiscussionFooter: React.FC<DiscussionFooterProps> = ({ discussionData }) => {
+const DiscussionFooter: React.FC<DiscussionFooterProps> = ({
+	discussionData,
+	discussionOptionsStateValue,
+	handleDiscussionOptions,
+	handleFooterShareClick,
+}) => {
 	return (
 		<div className="flex flex-row items-center h-10 rounded-b-lg p-1 relative">
 			<div className="flex flex-row items-center h-full flex-1">
@@ -30,6 +42,7 @@ const DiscussionFooter: React.FC<DiscussionFooterProps> = ({ discussionData }) =
 						type="button"
 						title="Share"
 						className="h-full flex flex-row items-center justify-center py-2 px-4 gap-x-2 rounded-lg text-gray-500 text-xs font-semibold"
+						onClick={() => handleDiscussionOptions("share")}
 					>
 						<div className="h-6 w-6 aspect-square">
 							<RiShareForwardLine className="h-full w-full" />
@@ -40,6 +53,13 @@ const DiscussionFooter: React.FC<DiscussionFooterProps> = ({ discussionData }) =
 					</button>
 				)}
 			</div>
+			{discussionData.discussion.privacy !== "private" && (
+				<DiscussionShareMenu
+					discussionData={discussionData}
+					discussionOptionsStateValue={discussionOptionsStateValue}
+					handleFooterShareClick={handleFooterShareClick}
+				/>
+			)}
 		</div>
 	);
 };

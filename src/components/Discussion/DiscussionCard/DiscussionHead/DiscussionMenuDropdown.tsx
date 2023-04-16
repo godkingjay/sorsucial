c69c419c6222771fdce5fd16cb4258b296ec6ba4
Jarrian <glorianagojar@gmail.com>
2023-04-16 09:43:06 +0800

    feat:    Add DiscussionMenuDropdown component with styles

---
+++ src/components/Discussion/DiscussionCard/DiscussionHead/DiscussionMenuDropdown.tsx
@@ -0,0 +1,62 @@
+import { DiscussionData, DiscussionOptionsState } from "@/atoms/discussionAtom";
+import { UserState } from "@/atoms/userAtom";
+import React from "react";
+import { BsThreeDots } from "react-icons/bs";
+import { MdDeleteOutline } from "react-icons/md";
+
+type DiscussionMenuDropdownProps = {
+	discussionData: DiscussionData;
+	discussionOptionsStateValue: DiscussionOptionsState;
+	handleDiscussionOptions: (name: keyof DiscussionOptionsState) => void;
+	handleDeleteDiscussion: () => void;
+};
+
+const DiscussionMenuDropdown: React.FC<DiscussionMenuDropdownProps> = ({
+	discussionData,
+	discussionOptionsStateValue,
+	handleDiscussionOptions,
+	handleDeleteDiscussion,
+}) => {
+	return (
+		<>
+			<button
+				type="button"
+				title="Discussion Menu"
+				className="discussion-menu-button"
+				onClick={() => handleDiscussionOptions("menu")}
+			>
+				<BsThreeDots className="h-full w-full" />
+			</button>
+			<div
+				className="discussion-dropdown-menu-wrapper"
+				menu-open={
+					discussionOptionsStateValue.menu === discussionData.discussion.id
+						? "true"
+						: "false"
+				}
+			>
+				<div className="discussion-dropdown-menu !max-h-[384px]">
+					<ul className="discussion-dropdown-list">
+						<li>
+							<button
+								type="button"
+								title="Delete Post"
+								className="discussion-dropdown-item hover:!text-red-500 hover:!bg-red-50 focus:!text-red-500 focus:!bg-red-50"
+								onClick={handleDeleteDiscussion}
+							>
+								<div className="icon-container">
+									<MdDeleteOutline className="icon" />
+								</div>
+								<div className="label-container">
+									<p className="label">Delete Discussion</p>
+								</div>
+							</button>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default DiscussionMenuDropdown;

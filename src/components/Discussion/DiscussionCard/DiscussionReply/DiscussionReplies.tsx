b7dd266dfaa92f9c742315955b6a312242f6f4a7
Jarrian <glorianagojar@gmail.com>
2023-04-19 09:21:47 +0800

    feat:    Add ReplyBox component and associated functions for replying to a discussion

--- src/components/Discussion/DiscussionCard/DiscussionReply/DiscussionReplies.tsx
+++ src/components/Discussion/DiscussionCard/DiscussionReply/DiscussionReplies.tsx
@@ -1,22 +1,118 @@
 import { UserState } from "@/atoms/userAtom";
 import React from "react";
 import ReplyBox from "./ReplyBox";
+import { Reply } from "@/lib/interfaces/discussion";
+import { DiscussionState } from "@/atoms/discussionAtom";
 
 type DiscussionRepliesProps = {
 	userStateValue: UserState;
+	userMounted: boolean;
+	currentDiscussion: DiscussionState["currentDiscussion"];
+	replyBoxRef: React.RefObject<HTMLTextAreaElement>;
+	formatNumberWithSuffix: (number: number) => string;
+};
+
+export type DiscussionReplyFormType = {
+	discussionId: string;
+	groupId?: string;
+	replyText: string;
+	replyForId: string;
+	replyLevel: number;
 };
 
 const DiscussionReplies: React.FC<DiscussionRepliesProps> = ({
 	userStateValue,
+	userMounted,
+	currentDiscussion,
+	replyBoxRef,
+	formatNumberWithSuffix,
 }) => {
+	const [discussionReplyForm, setDiscussionReplyForm] =
+		React.useState<DiscussionReplyFormType>({
+			discussionId: currentDiscussion?.discussion.id!,
+			groupId: currentDiscussion?.discussion.groupId,
+			replyText: "",
+			replyLevel: 0,
+			replyForId: currentDiscussion?.discussion.id!,
+		});
+	const [creatingReply, setCreatingReply] = React.useState(false);
+	const [firstLoadingReplies, setFirstLoadingReplies] = React.useState(false);
+	const [loadingReplies, setLoadingReplies] = React.useState(true);
+	const componentDidMount = React.useRef(false);
+
+	const handleReplySubmit = async (
+		event: React.FormEvent<HTMLFormElement>,
+		replyForm: DiscussionReplyFormType,
+		setReplyForm: React.Dispatch<React.SetStateAction<DiscussionReplyFormType>>,
+		replyForId: string,
+		replyLevel: number
+	) => {
+		event.preventDefault();
+
+		if (creatingReply) return;
+
+		setCreatingReply(true);
+
+		try {
+			// await createComment(
+			// 	{
+			// 		...replyForm,
+			// 		replyForId,
+			// 		replyLevel,
+			// 	},
+			// 	userStateValue.user
+			// );
+			setReplyForm((prev) => ({
+				...prev,
+				replyText: "",
+			}));
+		} catch (error) {
+			console.log("Hook: Error while creating comment: ", error);
+		}
+
+		setCreatingReply(false);
+	};
+
+	const handleInputChange = (
+		event: React.ChangeEvent<HTMLTextAreaElement>,
+		setReplyForm: React.Dispatch<React.SetStateAction<DiscussionReplyFormType>>
+	) => {
+		setReplyForm((prev) => ({
+			...prev,
+			replyText: event.target.value,
+		}));
+	};
+
 	return (
 		<>
-			<div className="h-[1px] bg-gray-200"></div>
-			<div className="p-4 flex flex-col gap-y-4">
-				<div className="flex flex-col gap-y-2">
-					<ReplyBox userStateValue={userStateValue} />
-				</div>
-			</div>
+			{currentDiscussion && (
+				<>
+					<div className="h-[1px] bg-gray-200"></div>
+					<div className="p-4 flex flex-col gap-y-2">
+						{firstLoadingReplies || !userMounted ? (
+							<>
+								<p>Loading Replies</p>
+							</>
+						) : (
+							<>
+								{currentDiscussion.discussion.isOpen && (
+									<ReplyBox
+										userStateValue={userStateValue}
+										replyForm={discussionReplyForm}
+										setReplyForm={setDiscussionReplyForm}
+										replyLevel={0}
+										replyForId={currentDiscussion.discussion.id}
+										submitting={creatingReply}
+										replyBoxRef={replyBoxRef}
+										onSubmit={handleReplySubmit}
+										onChange={handleInputChange}
+									/>
+								)}
+							</>
+						)}
+					</div>
+				</>
+			)}
 		</>
 	);
 };

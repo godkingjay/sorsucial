c1a67bc20befb2e5657fa1f257c0a1f3c5bbf783
Jarrian <glorianagojar@gmail.com>
2023-04-22 09:04:08 +0800

    feat:    Add userProfilePhotosCollection to userDb

--- src/lib/db/userDb.ts
+++ src/lib/db/userDb.ts
@@ -3,10 +3,13 @@ import clientPromise from "../mongodb";
 export default async function userDb() {
 	const client = await clientPromise;
 	const db = client.db("sorsu-db");
-	const usersCollection = db.collection("users");
 	const apiKeysCollection = db.collection("api-keys");
+	const usersCollection = db.collection("users");
+	const userProfilePhotosCollection = db.collection("user-profile-photos");
+
 	return {
-		usersCollection,
 		apiKeysCollection,
+		usersCollection,
+		userProfilePhotosCollection,
 	};
 }

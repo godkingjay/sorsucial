f7360b49f0988860bd5604a68305a557f76a9e16
Jarrian <glorianagojar@gmail.com>
2023-04-23 10:38:20 +0800

    feat: Add user state logic to the GroupsPage component

--- src/pages/groups/index.tsx
+++ src/pages/groups/index.tsx
@@ -1,16 +1,36 @@
+import LimitedBodyLayout from "@/components/Layout/LimitedBodyLayout";
+import useUser from "@/hooks/useUser";
 import Head from "next/head";
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect, useRef } from "react";
 
 type GroupsPageProps = {};
 
 const GroupsPage: React.FC<GroupsPageProps> = () => {
+	const { userStateValue, userMounted } = useUser();
+	const groupsMounted = useRef(false);
+	const router = useRouter();
+
+	useEffect(() => {
+		if (userMounted) {
+			if (!groupsMounted.current) {
+				groupsMounted.current = true;
+				console.log("GroupsPage: Mounted");
+			} else {
+				groupsMounted.current = true;
+			}
+		}
+	}, [userMounted]);
+
 	return (
 		<>
 			<Head>
 				<title>Groups | SorSUcial</title>
 			</Head>
-			<main>
-				<p>GroupsPage</p>
+			<main className="flex flex-col flex-1 p-4">
+				<LimitedBodyLayout>
+					<p>GroupsPage</p>
+				</LimitedBodyLayout>
 			</main>
 		</>
 	);

7784711d88e6c9783b3db3b79a21c9850a55cdeb
Jarrian <glorianagojar@gmail.com>
2023-04-25 15:10:27 +0800

    feat:   Update Tag interface to include groups property and rename existing properties to be more concise

--- src/lib/interfaces/tag.ts
+++ src/lib/interfaces/tag.ts
@@ -13,9 +13,10 @@
  * @property {string} id - The tag's id.
  * @property {string} name - The tag's name.
  * @property {string} [description] - The tag's description.
- * @property {number} totalCount - The total number of posts and discussions that are tagged with this tag.
- * @property {number} postCount - The number of posts that are tagged with this tag.
- * @property {number} discussionCount - The number of discussions that are tagged with this tag.
+ * @property {number} total - The total number of posts and discussions that are tagged with this tag.
+ * @property {number} posts - The number of posts that are tagged with this tag.
+ * @property {number} discussions - The number of discussions that are tagged with this tag.
+ * @property {number} groups - The number of groups that are tagged with this tag.
  * @property {Date} updatedAt - The date and time the tag was last changed.
  * @property {Date} createdAt - The date and time the tag was created.
  *
@@ -27,9 +28,10 @@ export interface Tag {
 	id: string;
 	name: string;
 	description?: string;
-	totalCount: number;
-	postCount: number;
-	discussionCount: number;
+	total: number;
+	posts: number;
+	discussions: number;
+	groups: number;
 	updatedAt: Date;
 	createdAt: Date;
 }

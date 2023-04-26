c2a87cadd7ca85e52ced4baf3bebb0cbc5a6d373
Jarrian <glorianagojar@gmail.com>
2023-04-26 09:36:19 +0800

    feat:    Add optional title to AddTags component

--- src/components/Form/Tag/AddTags.tsx
+++ src/components/Form/Tag/AddTags.tsx
@@ -2,12 +2,18 @@ import { useState } from "react";
 import { IoAddCircleOutline, IoRemoveCircleOutline } from "react-icons/io5";
 
 interface AddTagsProps {
+	title?: string;
 	items: string[];
 	setItems: React.Dispatch<React.SetStateAction<string[]>>;
 	itemName: string;
 }
 
-const AddTags: React.FC<AddTagsProps> = ({ items, setItems, itemName }) => {
+const AddTags: React.FC<AddTagsProps> = ({
+	title,
+	items,
+	setItems,
+	itemName,
+}) => {
 	const [inputItem, setInputItem] = useState<string>("");
 
 	const handleItemInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,44 +44,51 @@ const AddTags: React.FC<AddTagsProps> = ({ items, setItems, itemName }) => {
 	};
 
 	return (
-		<div className="flex flex-wrap gap-2">
-			{items.map((item, index) => (
-				<div
-					key={index}
-					className="flex items-center space-x-2 bg-blue-100 rounded-full px-2 py-1"
-				>
-					<span className="text-sm px-1">{item}</span>
+		<div className="flex w-full flex-col gap-y-2 p-2 relative border border-gray-200 bg-gray-50 rounded-lg">
+			{title && (
+				<div>
+					<p className="font-semibold text-sm text-gray-500">{title}</p>
+				</div>
+			)}
+			<div className="flex flex-wrap gap-2">
+				{items.map((item, index) => (
+					<div
+						key={index}
+						className="flex items-center space-x-2 bg-blue-100 rounded-full px-2 py-1"
+					>
+						<span className="text-sm px-1">{item}</span>
+						<button
+							type="button"
+							title={`Remove ${itemName}`}
+							className="text-gray-500 hover:text-red-500 focus:text-red-500"
+							onClick={() => handleRemoveItem(item)}
+						>
+							<div className="h-6 w-6 aspect-square">
+								<IoRemoveCircleOutline className="h-full w-full" />
+							</div>
+						</button>
+					</div>
+				))}
+				<div className="relative">
+					<input
+						name={`add-${itemName}`}
+						type="text"
+						placeholder={`Add ${itemName}`}
+						value={inputItem}
+						onChange={handleItemInputChange}
+						className="border border-gray-400 rounded-full px-4 pr-8 py-2 w-40 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
+					/>
 					<button
+						title="Add"
 						type="button"
-						title={`Remove ${itemName}`}
-						className="text-gray-500 hover:text-red-500 focus:text-red-500"
-						onClick={() => handleRemoveItem(item)}
+						className="absolute right-2 top-[50%] translate-y-[-50%] text-gray-300 rounded-full text-sm font-medium hover:text-blue-500 focus:text-blue-500"
+						onClick={handleAddItem}
 					>
 						<div className="h-6 w-6 aspect-square">
-							<IoRemoveCircleOutline className="h-full w-full" />
+							<IoAddCircleOutline className="h-full w-full" />
 						</div>
 					</button>
 				</div>
-			))}
-			<div className="relative">
-				<input
-					name={`add-${itemName}`}
-					type="text"
-					placeholder={`Add ${itemName}`}
-					value={inputItem}
-					onChange={handleItemInputChange}
-					className="border border-gray-400 rounded-full px-4 pr-8 py-2 w-40 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500"
-				/>
-				<button
-					title="Add"
-					type="button"
-					className="absolute right-2 top-[50%] translate-y-[-50%] text-gray-300 rounded-full text-sm font-medium hover:text-blue-500 focus:text-blue-500"
-					onClick={handleAddItem}
-				>
-					<div className="h-6 w-6 aspect-square">
-						<IoAddCircleOutline className="h-full w-full" />
-					</div>
-				</button>
 			</div>
 		</div>
 	);

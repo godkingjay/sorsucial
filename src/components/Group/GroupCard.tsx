4074ebe84eddb2d7a3a03462d79eb02b77cbb5b6
Jarrian <glorianagojar@gmail.com>
2023-04-28 11:37:04 +0800

    fix: some fixes to Group Card

--- src/components/Group/GroupCard.tsx
+++ src/components/Group/GroupCard.tsx
@@ -17,10 +17,10 @@ import { GoCommentDiscussion } from "react-icons/go";
 
 type GroupCardProps = {
 	groupData: GroupData;
-	sortBy: QueryGroupsSortBy;
+	index: number;
 };
 
-const GroupCard: React.FC<GroupCardProps> = ({ groupData, sortBy }) => {
+const GroupCard: React.FC<GroupCardProps> = ({ groupData, index }) => {
 	const router = useRouter();
 
 	const formatNumberWithSuffix = (number: number) => {
@@ -36,97 +36,105 @@ const GroupCard: React.FC<GroupCardProps> = ({ groupData, sortBy }) => {
 	};
 
 	return (
-		<div
-			className="
-        shadow-page-box-1 bg-white rounded-lg entrance-animation-slide-from-right relative
-        md:data-[top='true']:col-span-2
-      "
-			data-top={groupData.index[sortBy] < 3}
-			data-order={groupData.index[sortBy] + 1}
-		>
-			<div className="p-4 flex flex-row flex-wrap justify-center gap-x-4">
-				<div className="w-24 h-24 rounded-lg bg-gray-100 aspect-square shadow-lg overflow-hidden relative">
-					{groupData.group.image ? (
-						<Image
-							src={groupData.group.image.fileURL}
-							alt="Group Image"
-							className="w-full bg-center object-cover"
-							fill
-						/>
-					) : (
-						<div className="h-full w-full bg-gradient-to-t from-logo-400 to-logo-100 p-4 text-white">
-							<HiUserGroup className="h-full w-full" />
+		<div className="md:data-[top='true']:col-span-2">
+			<div
+				className="shadow-page-box-1 bg-white rounded-lg entrance-animation-slide-from-right relative"
+				data-top={index < 3}
+				data-order={index + 1}
+			>
+				<div className="p-4 flex flex-row flex-wrap justify-center gap-x-4">
+					<div className="w-24 h-24 rounded-lg bg-gray-100 aspect-square shadow-lg overflow-hidden relative">
+						{groupData.group.image ? (
+							<Image
+								src={groupData.group.image.fileURL}
+								alt="Group Image"
+								className="w-full bg-center object-cover"
+								fill
+							/>
+						) : (
+							<div className="h-full w-full bg-gradient-to-t from-logo-400 to-logo-100 p-4 text-white">
+								<HiUserGroup className="h-full w-full" />
+							</div>
+						)}
+					</div>
+					<div className="flex flex-col flex-1">
+						<div className="flex flex-row w-full">
+							<Link
+								href={`/groups/${groupData.group.id}`}
+								title={groupData.group.name}
+							>
+								<h2 className="truncate text-lg font-semibold text-gray-700">
+									{groupData.group.name}
+								</h2>
+							</Link>
 						</div>
-					)}
-				</div>
-				<div className="flex flex-col flex-1">
-					<div className="flex flex-row w-full">
-						<Link
-							href={`/groups/${groupData.group.id}`}
-							title={groupData.group.name}
-						>
-							<h2 className="truncate text-lg font-semibold text-gray-700">
-								{groupData.group.name}
-							</h2>
-						</Link>
-					</div>
-					<div className="flex flex-row gap-x-2">
-						<p className="text-2xs text-gray-500 inline-flex items-center gap-x-1">
-							Created by{" "}
-							<span className="inline-flex flex-row h-4 w-4">
-								<UserIcon user={groupData.creator} />
-							</span>
-							•
-							<span className="text-2xs text-gray-500 truncate">
-								{moment(groupData.group.createdAt).diff(moment(), "days") > -7
-									? moment(groupData.group.createdAt).fromNow()
-									: moment(groupData.group.createdAt).format("MMMM DD, YYYY")}
-							</span>
-						</p>
-					</div>
-					<div className="group-card-details-wrapper">
-						<div className="group-card-details-container">
-							<div
-								className="group-detail total-members"
-								title="Members"
-								has-value={
-									groupData.group.numberOfMembers !== 0 ? "true" : "false"
-								}
-							>
-								<div className="icon-container">
-									<BsFillPersonFill className="icon" />
+						<div className="flex flex-row gap-x-2">
+							<div className="text-2xs text-gray-500 inline-flex items-center gap-x-1">
+								Created by{" "}
+								<div className="inline-flex flex-row h-4 w-4">
+									<UserIcon user={groupData.creator} />
 								</div>
-								<p className="label">
-									{formatNumberWithSuffix(groupData.group.numberOfMembers)}
+								•
+								<p className="text-2xs text-gray-500 truncate">
+									{moment(groupData.group.createdAt).diff(moment(), "days") > -7
+										? moment(groupData.group.createdAt).fromNow()
+										: moment(groupData.group.createdAt).format("MMMM DD, YYYY")}
 								</p>
 							</div>
-							<div
-								className="group-detail total-posts"
-								title="Posts"
-								has-value={
-									groupData.group.numberOfPosts !== 0 ? "true" : "false"
-								}
-							>
-								<div className="icon-container">
-									<BsFillFileEarmarkTextFill className="icon" />
-								</div>
-								<p className="label">
-									{formatNumberWithSuffix(groupData.group.numberOfPosts)}
+						</div>
+						{groupData.group.description && (
+							<div className="flex flex-col mt-2">
+								<p className="text-xs text-gray-500">
+									{groupData.group.description.length > 128
+										? groupData.group.description.substring(0, 128).concat("...")
+										: groupData.group.description}
 								</p>
 							</div>
-							<div
-								className="group-detail total-discussions"
-								title="Discussions"
-								has-value={
-									groupData.group.numberOfDiscussions !== 0 ? "true" : "false"
-								}
-							>
-								<div className="icon-container">
-									<GoCommentDiscussion className="icon" />
+						)}
+						<div className="group-card-details-wrapper">
+							<div className="group-card-details-container">
+								<div
+									className="group-detail total-members"
+									title="Members"
+									has-value={
+										groupData.group.numberOfMembers !== 0 ? "true" : "false"
+									}
+								>
+									<div className="icon-container">
+										<BsFillPersonFill className="icon" />
+									</div>
+									<p className="label">
+										{formatNumberWithSuffix(groupData.group.numberOfMembers)}
+									</p>
 								</div>
-								<p className="label">
-									{formatNumberWithSuffix(groupData.group.numberOfDiscussions)}
-								</p>
+								<div
+									className="group-detail total-posts"
+									title="Posts"
+									has-value={
+										groupData.group.numberOfPosts !== 0 ? "true" : "false"
+									}
+								>
+									<div className="icon-container">
+										<BsFillFileEarmarkTextFill className="icon" />
+									</div>
+									<p className="label">
+										{formatNumberWithSuffix(groupData.group.numberOfPosts)}
+									</p>
+								</div>
+								<div
+									className="group-detail total-discussions"
+									title="Discussions"
+									has-value={
+										groupData.group.numberOfDiscussions !== 0 ? "true" : "false"
+									}
+								>
+									<div className="icon-container">
+										<GoCommentDiscussion className="icon" />
+									</div>
+									<p className="label">
+										{formatNumberWithSuffix(groupData.group.numberOfDiscussions)}
+									</p>
+								</div>
 							</div>
 						</div>
 					</div>

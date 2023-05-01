f391e8e3b20b1c8f0bd7fa0a00601b1de36c165e
Jarrian <glorianagojar@gmail.com>
2023-05-01 18:37:31 +0800

    feat: postFilter with creator

--- src/components/Post/PostsFilter.tsx
+++ src/components/Post/PostsFilter.tsx
@@ -58,6 +58,13 @@ const PostsFilter: React.FC<PostsFilterProps> = ({
 			postStateValue.posts.filter(
 				(post) =>
 					(groupId ? post.post.groupId === groupId : true) &&
+					(creator
+						? post.creator?.uid.match(regexCreator) ||
+						  post.creator?.firstName.match(regexCreator) ||
+						  post.creator?.lastName.match(regexCreator) ||
+						  post.creator?.middleName?.match(regexCreator) ||
+						  post.creator?.email.match(regexCreator)
+						: true) &&
 					post.post.privacy === privacy &&
 					post.post.postType === postType &&
 					post.index &&
@@ -65,7 +72,15 @@ const PostsFilter: React.FC<PostsFilterProps> = ({
 					post.index[sortBy] >= 0
 			)
 		);
-	}, [postStateValue.posts, groupId, privacy, postType, sortBy]);
+	}, [
+		postStateValue.posts,
+		groupId,
+		creator,
+		regexCreator,
+		privacy,
+		postType,
+		sortBy,
+	]);
 
 	const handleFetchPosts = useCallback(async () => {
 		setLoadingPosts(true);

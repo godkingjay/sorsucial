24a130800ac4772d29c1be42b87a568e04e7b1e0
Jarrian <glorianagojar@gmail.com>
2023-05-03 17:51:33 +0800

    feat: remove unused imports

--- src/pages/user/[userId]/index.tsx
+++ src/pages/user/[userId]/index.tsx
@@ -1,15 +1,9 @@
 import { UserState } from "@/atoms/userAtom";
-import UserIcon from "@/components/Icons/UserIcon";
-import LimitedBodyLayout from "@/components/Layout/LimitedBodyLayout";
-import LoadingScreen from "@/components/Skeleton/LoadingScreen";
 import UserPageLoader from "@/components/User/UserPageLoader";
 import useUser from "@/hooks/useUser";
 import clientPromise from "@/lib/mongodb";
 import { GetServerSidePropsContext } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import React, { useEffect } from "react";
-import { FaUserCircle } from "react-icons/fa";
+import React from "react";
 import safeJsonStringify from "safe-json-stringify";
 
 type UserProfileProps = {
@@ -29,49 +23,6 @@ const UserProfilePage: React.FC<UserProfileProps> = ({
 				userPageData={userPageData}
 				loadingUser={loadingPage}
 			>
-				{/* {userStateValue.userPage && (
-					<div className="flex flex-col gap-y-2 py-2">
-						<div className="flex flex-col shadow-page-box-1 p-4 rounded-lg bg-white">
-							<div className="bg-gray-100 aspect-[2/1] rounded-lg overflow-hidden relative">
-								{userStateValue.userPage.user.coverImageURL ? (
-									<Image
-										src={userStateValue.userPage.user.coverImageURL}
-										alt="User Cover Image"
-										className="w-full bg-center object-cover"
-										sizes="(min-width: 1200px) 900px, (min-width: 768px) 700px, 100vw"
-										fill
-									/>
-								) : (
-									<div>Add Image</div>
-								)}
-							</div>
-							<div className="flex flex-col">
-								<div className="flex flex-row h-16 xs:h-20 sm:h-28 gap-x-4">
-									<div className="border-2 border-white bg-white flex flex-row h-20 w-20 duration-100 xs:h-28 xs:w-28 sm:h-36 sm:w-36 -translate-y-4 xs:-translate-y-8 aspect-square rounded-full overflow-hidden">
-										{userStateValue.userPage.user.imageURL ? (
-											<Image
-												src={userStateValue.userPage.user.imageURL}
-												alt="User Profile Image"
-												className="w-full bg-center object-cover"
-												sizes="(min-width: 1200px) 900px, (min-width: 768px) 700px, 100vw"
-												fill
-											/>
-										) : (
-											<FaUserCircle className="h-full w-full text-gray-200" />
-										)}
-									</div>
-									<div className="flex flex-col mt-1 xs:mt-2">
-										<h1 className="xs:text-lg sm:text-xl md:text-3xl font-bold">
-											{userStateValue.userPage.user.firstName}{" "}
-											{userStateValue.userPage.user.lastName}
-										</h1>
-									</div>
-								</div>
-							</div>
-						</div>
-						<p>Body</p>
-					</div>
-				)} */}
 				<div>Hello</div>
 			</UserPageLoader>
 		</>

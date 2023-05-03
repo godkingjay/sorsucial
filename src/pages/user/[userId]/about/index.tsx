44b9a4fe0c0feae51bbdc402ed81534b657ed581
Jarrian <glorianagojar@gmail.com>
2023-05-03 17:58:37 +0800

    feat: user about page

---
+++ src/pages/user/[userId]/about/index.tsx
@@ -0,0 +1,56 @@
+import { UserState } from "@/atoms/userAtom";
+import UserPageLoader from "@/components/User/UserPageLoader";
+import clientPromise from "@/lib/mongodb";
+import { GetServerSidePropsContext } from "next";
+import React from "react";
+import safeJsonStringify from "safe-json-stringify";
+
+type UserPageAboutPageProps = {
+	userPageData: UserState["userPage"];
+	loadingPage: boolean;
+};
+
+const UserPageAboutPage: React.FC<UserPageAboutPageProps> = ({
+	userPageData,
+	loadingPage = true,
+}) => {
+	return (
+		<>
+			<UserPageLoader
+				userPageData={userPageData}
+				loadingUser={loadingPage}
+			>
+				<div>User About Page</div>
+			</UserPageLoader>
+		</>
+	);
+};
+
+export default UserPageAboutPage;
+
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+	try {
+		const client = await clientPromise;
+		const db = client.db("sorsu-db");
+		const usersCollection = db.collection("users");
+		const { userId } = context.query;
+
+		const userPageData = {
+			user: await usersCollection.findOne({ uid: userId }),
+		};
+
+		return {
+			props: {
+				userPageData: userPageData.user
+					? JSON.parse(safeJsonStringify(userPageData))
+					: null,
+				loadingPage: false,
+			},
+		};
+	} catch (error: any) {
+		console.log("USER PAGE: getServerSideProps Error: ", error.message);
+		return {
+			notFound: true,
+		};
+	}
+};

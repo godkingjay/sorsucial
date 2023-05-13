ab08b535481d6fd6b062d46b9d287aa75f6a4442
Jarrian <glorianagojar@gmail.com>
2023-05-13 17:19:21 +0800

    feat: loading when uploading photo

--- src/components/Group/GroupPageHeader/GroupProfileMenu.tsx
+++ src/components/Group/GroupPageHeader/GroupProfileMenu.tsx
@@ -6,6 +6,7 @@ import { validImageTypes } from "@/lib/types/validFiles";
 import Link from "next/link";
 import React, { useCallback, useRef, useState } from "react";
 import { BsPersonBoundingBox, BsThreeDots } from "react-icons/bs";
+import { FiLoader } from "react-icons/fi";
 import { IoSettingsOutline } from "react-icons/io5";
 import { MdInsertPhoto } from "react-icons/md";
 
@@ -19,8 +20,7 @@ const GroupProfileMenu: React.FC<GroupProfileMenuProps> = ({ groupData }) => {
 
 	const { uploadImageOrVideo } = useInput();
 
-	const [uploadingGroupPhoto, setUploadingGroupPhoto] = useState(false);
-	const [uploadingCoverPhoto, setUploadingCoverPhoto] = useState(false);
+	const [uploadingPhoto, setUploadingPhoto] = useState(false);
 
 	const uploadProfilePhotoRef = useRef<HTMLInputElement>(null);
 	const uploadCoverPhotoRef = useRef<HTMLInputElement>(null);
@@ -41,47 +41,26 @@ const GroupProfileMenu: React.FC<GroupProfileMenuProps> = ({ groupData }) => {
 
 	const handleUploadPhoto = useCallback(
 		async (event: React.ChangeEvent<HTMLInputElement>) => {
-			const { name: type } = event.target;
-
 			try {
-				if (
-					((type === "image" && !uploadingGroupPhoto) ||
-						(type === "cover" && !uploadingCoverPhoto)) &&
-					event.target.files &&
-					event.target.files[0]
-				) {
-					if (type === "image") {
-						setUploadingGroupPhoto(true);
-					} else if (type === "cover") {
-						setUploadingCoverPhoto(true);
-					}
-
+				if (!uploadingPhoto && event.target.files && event.target.files[0]) {
+					setUploadingPhoto(true);
 					const image = await uploadImageOrVideo(event.target.files[0]);
 
 					if (image) {
 						await changePhoto({
 							image,
-							type: type as GroupImage["type"],
+							type: event.target.name as GroupImage["type"],
 						});
 					}
 
-					if (type === "image") {
-						setUploadingGroupPhoto(false);
-					} else if (type === "cover") {
-						setUploadingCoverPhoto(false);
-					}
+					setUploadingPhoto(false);
 				}
 			} catch (error: any) {
 				console.log(`=>Hook: Change User Photo Error:\n${error.message}`);
-
-				if (type === "image") {
-					setUploadingGroupPhoto(false);
-				} else if (type === "cover") {
-					setUploadingCoverPhoto(false);
-				}
+				setUploadingPhoto(false);
 			}
 		},
-		[changePhoto, uploadImageOrVideo, uploadingCoverPhoto, uploadingGroupPhoto]
+		[changePhoto, uploadImageOrVideo, uploadingPhoto]
 	);
 
 	return (
@@ -106,46 +85,57 @@ const GroupProfileMenu: React.FC<GroupProfileMenuProps> = ({ groupData }) => {
 					group-data-[open=false]:pointer-events-none group-data-[open=false]:opacity-0 group-data-[open=false]:translate-y-[8px]
 				"
 				>
-					<button
-						type="button"
-						title="Change Profile Photo"
-						className="user-menu-item"
-						tabIndex={
-							groupOptionsStateValue.menu === groupData.group.id ? 0 : -1
-						}
-						onClick={() =>
-							!uploadingGroupPhoto &&
-							!uploadingCoverPhoto &&
-							uploadProfilePhotoRef.current?.click()
-						}
-					>
-						<div className="icon-container">
-							<BsPersonBoundingBox className="icon" />
-						</div>
-						<div className="label-container">
-							<p className="label">Change Group Photo</p>
-						</div>
-					</button>
-					<button
-						type="button"
-						title="Change Cover Photo"
-						className="user-menu-item"
-						tabIndex={
-							groupOptionsStateValue.menu === groupData.group.id ? 0 : -1
-						}
-						onClick={() =>
-							!uploadingGroupPhoto &&
-							!uploadingCoverPhoto &&
-							uploadCoverPhotoRef.current?.click()
-						}
-					>
-						<div className="icon-container">
-							<MdInsertPhoto className="icon" />
-						</div>
-						<div className="label-container">
-							<p className="label">Change Cover Photo</p>
-						</div>
-					</button>
+					{!uploadingPhoto ? (
+						<>
+							<button
+								type="button"
+								title="Change Profile Photo"
+								className="user-menu-item"
+								tabIndex={
+									groupOptionsStateValue.menu === groupData.group.id ? 0 : -1
+								}
+								onClick={() =>
+									!uploadingPhoto && uploadProfilePhotoRef.current?.click()
+								}
+							>
+								<div className="icon-container">
+									<BsPersonBoundingBox className="icon" />
+								</div>
+								<div className="label-container">
+									<p className="label">Change Group Photo</p>
+								</div>
+							</button>
+							<button
+								type="button"
+								title="Change Cover Photo"
+								className="user-menu-item"
+								tabIndex={
+									groupOptionsStateValue.menu === groupData.group.id ? 0 : -1
+								}
+								onClick={() =>
+									!uploadingPhoto && uploadCoverPhotoRef.current?.click()
+								}
+							>
+								<div className="icon-container">
+									<MdInsertPhoto className="icon" />
+								</div>
+								<div className="label-container">
+									<p className="label">Change Cover Photo</p>
+								</div>
+							</button>
+						</>
+					) : (
+						<>
+							<div className="user-menu-item justify-center text-blue-500 animate-pulse pointer-events-none">
+								<div className="icon-container animate-spin">
+									<FiLoader className="icon" />
+								</div>
+								<div className="label-container">
+									<p className="label">Uploading Photo</p>
+								</div>
+							</div>
+						</>
+					)}
 					<Link
 						href={`/user/${groupData.group.id}/settings/`}
 						title="Settings"

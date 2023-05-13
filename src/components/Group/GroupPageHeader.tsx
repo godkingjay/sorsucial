2bfa272607d0438fcca37d492b8e2282c6f21292
Jarrian <glorianagojar@gmail.com>
2023-05-13 13:58:14 +0800

    feat: GroupPageHeader to use GroupIcon component

--- src/components/Group/GroupPageHeader.tsx
+++ src/components/Group/GroupPageHeader.tsx
@@ -8,6 +8,7 @@ import { UserState } from "@/atoms/userAtom";
 import { HiOutlineCamera } from "react-icons/hi";
 import Link from "next/link";
 import useGroup from "@/hooks/useGroup";
+import GroupIcon from "../Icons/GroupIcon";
 
 type GroupPageHeaderProps = {
 	groupData: GroupData;
@@ -53,43 +54,7 @@ const GroupPageHeader: React.FC<GroupPageHeaderProps> = ({
 				</div>
 				<div className="h-32 p-4 flex flex-row gap-x-4">
 					<div className="relative border-4 border-white flex md:ml-8 -translate-y-8 sm:-translate-y-12 h-28 w-28 sm:h-36 sm:w-36 aspect-square rounded-full bg-gray-100">
-						{groupData.group.image ? (
-							<>
-								<Image
-									src={groupData.group.image.fileURL}
-									alt={groupData.group.name}
-									sizes="(min-width: 1200px) 900px, (min-width: 768px) 700px, 100vw"
-									fill
-									className="w-full bg-center object-cover rounded-full"
-								/>
-							</>
-						) : (
-							<div className="w-full h-full overflow-hidden rounded-full bg-gray-100 relative">
-								<div className="h-full w-full aspect-square text-gray-300 scale-125">
-									<RiGroup2Fill className="h-full w-full" />
-								</div>
-								{!groupData.group.image && (
-									<>
-										{(userStateValue.user?.roles.includes("admin") ||
-											groupData.userJoin?.roles.includes("owner") ||
-											groupData.userJoin?.roles.includes("admin")) && (
-											<button
-												type="button"
-												title="Add Photo"
-												className="opacity-0 hover:opacity-100 duration-100 bg-slate-400 bg-opacity-25 backdrop-filter backdrop-blur-sm absolute top-0 left-0 flex flex-col gap-y-2 h-full w-full text-white items-center justify-center p-4 hover:text-blue-400"
-											>
-												<div className="h-8 w-8 aspect-square">
-													<HiOutlineCamera className="h-full w-full" />
-												</div>
-												<div>
-													<p className="text-sm">Add Photo</p>
-												</div>
-											</button>
-										)}
-									</>
-								)}
-							</div>
-						)}
+						<GroupIcon group={groupData.group} />
 					</div>
 					<div className="flex-1 flex flex-col">
 						<div className="w-full flex flex-row">
---
+++ src/components/Icons/GroupIcon.tsx
@@ -0,0 +1,45 @@
+import { SiteGroup } from "@/lib/interfaces/group";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { RiGroup2Fill } from "react-icons/ri";
+
+type GroupIconProps = {
+	group: SiteGroup | null;
+	disabled?: boolean;
+};
+
+const GroupIcon: React.FC<GroupIconProps> = ({ group, disabled = false }) => {
+	return (
+		<>
+			{group ? (
+				<Link
+					href={`/groups/${group.id}`}
+					title={`${group.name}`}
+					className="h-full w-full flex relative aspect-square rounded-full overflow-hidden border border-transparent text-gray-300 data-[disabled=true]:pointer-events-none"
+					data-disabled={disabled}
+					tabIndex={disabled ? -1 : 0}
+				>
+					{group.image?.fileURL ? (
+						<Image
+							src={group.image?.fileURL}
+							alt={group.name}
+							sizes="128px"
+							fill
+							loading="lazy"
+							className="w-full bg-center object-cover"
+						/>
+					) : (
+						<RiGroup2Fill className="h-full w-full" />
+					)}
+				</Link>
+			) : (
+				<div className="h-full w-full aspect-square rounded-full overflow-hidden border border-transparent text-gray-300">
+					<RiGroup2Fill className="h-full w-full" />
+				</div>
+			)}
+		</>
+	);
+};
+
+export default GroupIcon;

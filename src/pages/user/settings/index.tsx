13d4ebd2f1ff14867f3c17c065c70ce5cb951a59
Jarrian <glorianagojar@gmail.com>
2023-05-14 12:02:21 +0800

    feat: user page switch

---
+++ src/pages/user/settings/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type SettingsPageProps = {};
+
+const SettingsPage: React.FC<SettingsPageProps> = () => {
+	return <div>SettingsPage</div>;
+};
+
+export default SettingsPage;

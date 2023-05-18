503c065524d363f1813df47b3aaa67b344cbea89
Jarrian <glorianagojar@gmail.com>
2023-05-18 11:20:27 +0800

    feat: post or discussion not found component

--- src/components/Error/PostDiscussionNotFound.tsx
+++ src/components/Error/PostDiscussionNotFound.tsx
@@ -1,4 +1,8 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import { FaBullhorn } from "react-icons/fa";
+import { RxCaretRight } from "react-icons/rx";
 
 export type PostDiscussionNotFoundProps = {
 	type:
@@ -12,10 +16,73 @@ export type PostDiscussionNotFoundProps = {
 const PostDiscussionNotFound: React.FC<PostDiscussionNotFoundProps> = ({
 	type,
 }) => {
+	const router = useRouter();
+
+	const { groupId, userId } = router.query;
+
 	return (
-		<div className="flex flex-col items-center mx-4 my-8">
-			<p>PostDiscussionNotFound</p>
-		</div>
+		<>
+			<div className="p-4 flex flex-col items-center">
+				<div className="flex flex-col items-center max-w-sm my-4 text-gray-700 gap-y-4">
+					<div className="h-32 w-32 p-4 bg-white rounded-full shadow-page-box-1 border-4 border-gray-100">
+						<FaBullhorn className="h-full w-full" />
+					</div>
+					<div className="flex flex-col items-center gap-y-4">
+						<p className="font-bold">
+							This{" "}
+							{type === "user-post"
+								? "post"
+								: type === "announcement"
+								? "announcement"
+								: type === "group-post"
+								? "group post"
+								: type === "discussion"
+								? "discussion"
+								: type === "group-discussion"
+								? "group discussion"
+								: "content"}{" "}
+							could not be found!
+						</p>
+						<Link
+							href={
+								type === "user-post"
+									? `/user/${userId}/`
+									: type === "announcement"
+									? `/`
+									: type === "group-post"
+									? `/groups/${groupId}/`
+									: type === "discussion"
+									? `/discussions`
+									: type === "group-discussion"
+									? `/groups/${groupId}/discussions`
+									: `/`
+							}
+							className="border-2 flex flex-row gap-x-1 items-center border-blue-500 rounded-full px-2 pl-4 py-2 text-sm font-semibold text-blue-500 group hover:bg-blue-100 hover:text-blue-600 focus:bg-blue-100 focus:text-blue-600"
+						>
+							<div>
+								<p>
+									Go to{" "}
+									{type === "user-post"
+										? "User Page"
+										: type === "announcement"
+										? "Homepage"
+										: type === "group-post"
+										? "Group Posts"
+										: type === "discussion"
+										? "Discussions"
+										: type === "group-discussion"
+										? "Group Discussions"
+										: "Homepage"}
+								</p>
+							</div>
+							<div className="h-6 w-6 duration-200 group-hover:translate-x-1 group-focus:translate-x-1">
+								<RxCaretRight className="h-full w-full" />
+							</div>
+						</Link>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
